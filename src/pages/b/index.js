37c0ca89962eefaacabf5fc18b4eeaed3eba05a4
TangWJ <qreal829475@gmail.com>
2021-04-28 16:52:49 +0800

    初始化第一次

---
+++ src/pages/b/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class B extends Component {
+    render() {
+        return (
+            <div>
+                B
+            </div>
+        )
+    }
+}
+
+export default B;
